568ca934d2e915879a8731b33ea1353330ae9937
EmilVuchev <emo_vuchev_90@abv.bg>
2022-06-05 19:22:49 +0300

    Adds not working api controller

---
+++ Web/PriLalo.Web/Controllers/MealApiController.cs
@@ -0,0 +1,48 @@
+﻿namespace PriLalo.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+    using PriLalo.Services.Data.Cart;
+    using PriLalo.Services.Data.Meal;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MealApiController : Controller
+    {
+        private readonly IMealService mealService;
+        private readonly ICartService cartService;
+
+        public MealApiController(IMealService mealService, ICartService cartService)
+        {
+            this.mealService = mealService;
+            this.cartService = cartService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> Add(Neshto input)
+        {
+            //var meal = this.mealService.GetById<MealViewModel>(id);
+
+            //var cart = new List<CartItemViewModel>();
+
+            //var cartExists = this.HttpContext.Session.TryGetValue("cart", out byte[] result);
+
+            //if (cartExists)
+            //{
+            //    cart = this.cartService.DeserializeCartContent(result);
+            //}
+
+            //cart = this.cartService.AddItemToCart(meal, cart);
+
+            //var serializedCart = this.cartService.SerializeCartContent(cart);
+            //this.HttpContext.Session.Set("cart", serializedCart);
+            return "wazaaaa";
+        }
+    }
+
+    public class Neshto
+    {
+        public string Id { get; set; }
+    }
+}
--- Web/PriLalo.Web/wwwroot/js/menu.js
+++ Web/PriLalo.Web/wwwroot/js/menu.js
@@ -1,8 +1,53 @@
 ﻿let mealsToCart = function () {
-    document.querySelectorAll('.meal-to-cart').forEach(x => x.addEventListener("click", function (event) {
-        console.log(x.data-id);
-        
-    }));
+    document.querySelectorAll('.meal-to-cart')
+        .forEach(x => x.addEventListener("click", function (event) {
+
+            //    var xhr = new XMLHttpRequest();
+            //    xhr.open("POST", "/api/MealApi/", true);
+            //    xhr.setRequestHeader('Content-Type', 'application/json');
+            //    //xhr.send(JSON.stringify({
+            //    //    id: this.id
+            //    //}));
+
+            //    xhr.send(this.id);
+
+            //    xhr.onreadystatechange = function (result) {
+            //        if (result) {
+            //            alert(result);
+            //        }
+
+            //        if (xhr.readyState == XMLHttpRequest.DONE) {
+            //            alert("qkata rabota")
+            //            //var jsonResponse = JSON.parse(xhr.responseText)
+            //            //window.location.replace(jsonResponse.redirectRoute)
+            //        }
+            //    }
+
+            //}));
+
+            let data = { id: this.id }
+            debugger;
+            var antiForgeryToken = $('#antiForgeryForm input[name=__RequestVerificationToken]').val();
+            $.ajax({
+                type: "POST",
+                url: "/api/MealApi",
+                data: JSON.stringify(data),
+                dataType: 'json',
+                contentType: 'application/json',
+                headers: {
+                    'X-CSRF-TOKEN': antiForgeryToken
+                },
+                success: function (data) {
+
+                    //console.log(data);
+                    //updateResource(data);
+
+                    ////let newUnitQuantity = Number($(`#${data.unitType}`).text().split(`${data.unitType} `).filter(Boolean)) + data.unitQuantity;
+                    //$(`#${data.unitType}`).text(`${data.unitType} ${data.unitQuantity}`)
+                }
+            });
+        })
+        )
 }
 
 mealsToCart();
